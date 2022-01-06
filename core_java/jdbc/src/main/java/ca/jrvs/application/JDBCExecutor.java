0421dae846e593d3a3fcd80ca4516385fdfc3eae
Ashwin Rishi <pjashwinrishi@gmail.com>
2022-01-06 17:29:01 +0000

    prep updation 4.0

--- core_java/jdbc/src/main/java/ca/jrvs/application/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/application/JDBCExecutor.java
@@ -2,21 +2,21 @@ package ca.jrvs.application;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCExecutor {
     public static void main(String[] args) {
         DataBaseConnectionManager dataBaseConnectionManager = new DataBaseConnectionManager("localhost", "hplussport", "postgres", "password");
-        Customer customer;
+        List<Customer> customers = new ArrayList<>();
         try {
             Connection connection = dataBaseConnectionManager.getConnection();
             CustomerDAO customerDAO = new CustomerDAO(connection);
+            customerDAO.findAllSorted(20).forEach(customer -> {System.out.println(customer.getFirstName());});
 
-            customer = customerDAO.findById(10000);
-            System.out.println("before update:" +customer.getEmail());
-
-            customer.setEmail("newEmail@email.com");
-            customer = customerDAO.update(customer);
-            System.out.println("after update:" +customer.getEmail());
+            for(int i=1;i<3;i++){
+                customerDAO.findAllPaged(10, i).forEach(customer -> {System.out.println(customer.getEmail());});
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }

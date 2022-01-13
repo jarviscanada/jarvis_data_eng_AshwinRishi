bcaeb476b92a6a218b549cf143c7883c770414bc
Ashwin Rishi <pjashwinrishi@gmail.com>
2022-01-13 20:20:08 +0000

    twitter update version 1.0

--- core_java/Twitter/src/main/java/ca/jrvs/apps/Twitter/TwitterApplication.java
+++ core_java/Twitter/src/main/java/ca/jrvs/apps/Twitter/TwitterApplication.java
@@ -1,23 +1,17 @@
 package ca.jrvs.apps.Twitter;
 
-import java.io.IOException;
-import java.util.Arrays;
-
+import com.google.gdata.util.common.base.PercentEscaper;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
-import com.google.gdata.util.common.base.PercentEscaper;
-
-import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import java.io.IOException;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TwitterApplication {
@@ -38,8 +32,6 @@ public class TwitterApplication {
 
 		String status = "Testing from jarvis:";
 		percentEscaper = new PercentEscaper("", false);
-
-		System.out.println("consumer key:" + consumerKey);
 		HttpPost request = new HttpPost(
 				"https://api.twitter.com/1.1/statuses/update.json?status=" + percentEscaper.escape(status));
 
@@ -51,16 +43,13 @@ public class TwitterApplication {
 
 		System.out.println("Http Request headers");
 		Arrays.stream(request.getAllHeaders()).forEach(System.out::println);
-
 		httpClient = HttpClientBuilder.create().build();
-		HttpResponse response;
 		try {
 			response = httpClient.execute(request);
 			System.out.println("ouput:" + EntityUtils.toString(response.getEntity()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public static void main(String[] args) throws ClientProtocolException, IOException {
